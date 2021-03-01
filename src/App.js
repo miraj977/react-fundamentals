f3b67e72ca1ddcb6bb2ae5a6670db5ff5d4ee15a
Miraj <mirajaryal@gmail.com>
2021-03-02 09:47:16 +1100

    react-bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Todo from './Todo';
 
+
 function App ()
 {
   return (
--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
-
+import { Button, Form, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Todo = () =>
 {
@@ -13,19 +14,31 @@ const Todo = () =>
         renderCount.current = renderCount.current + 1;
     } );
     return (
-        <div className="todo-container">
+        <Container>
             <p className="render">Render Count: {renderCount.current}</p>
-
-            <input type="text" ref={inputRef} value={name} onChange={e =>
-            {
-                setName( e.target.value )
-            }} />
-            <input type="submit" value="Submit" />
-            <p>Counter: <span>{count}</span></p>
-            <button type="button" onClick={() => setCount( count + 1 )}>Increase</button>
-            <button type="button" onClick={() => setCount( 0 )}>Reset</button>
-            <p>New Name: {name}</p>
-        </div>
+            <Form>
+                <Form.Label className="inputTextLabel">Controlled Input</Form.Label>
+                <Form.Control type="text" ref={inputRef} value={name} onChange={( e ) =>
+                {
+                    setName( e.target.value );
+                }} ></Form.Control>
+                <Form.Text className="text-muted form-container">You typed... {name}</Form.Text>
+                <Form.Text className="text-muted form-container">The counter value is {count}</Form.Text>
+                <div className="d-flex form-container">
+                    <Row className="">
+                        <Col>
+                            <Button variant="secondary" onClick={() => { setCount( count - 1 ) }}>Decrease</Button>
+                        </Col>
+                        <Col>
+                            <Button variant="danger" onClick={() => { setCount( 0 ) }}>Reset</Button>
+                        </Col>
+                        <Col>
+                            <Button variant="secondary" onClick={() => { setCount( count + 1 ) }}>Increase</Button>
+                        </Col>
+                    </Row>
+                </div>
+            </Form>
+        </Container>
     )
 }
 export default Todo; 
